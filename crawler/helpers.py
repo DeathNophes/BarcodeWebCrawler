5e17c0d6ec69389f85e65ed33dc66b5cc67de712
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-05-25 00:33:00 +0300

    Upload helpers.py

---
+++ crawler/helpers.py
@@ -0,0 +1,8 @@
+
+pages_links_csv_path = '../data/page_links.csv'
+books_links_csv_path = '../data/books_links.csv'
+
+books_data_csv_path = '../data/books_data.csv'
+valid_barcodes_file_path = '../data/valid_barcodes.txt'
+
+admin_panel_link = ''
